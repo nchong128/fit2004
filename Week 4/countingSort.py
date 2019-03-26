fcad97c25f9ef3fd29d2ecddb73b1eda4cb785f8
nchong128 <nchong128@gmail.com>
2019-03-26 21:20:04 +1100

    Finish tutorial

--- Week 4/countingSort.py
+++ Week 4/countingSort.py
@@ -75,7 +75,8 @@ Problem 2
 a) Select the first element of the sequence
 - Element is chosen at random
 - Worst case is O(n^2) where n is the length of the list
-- Example case: Where the first element is either the smallest or the largest of the list
+- Example case: Where the list is in descending order
+    - first element picked will always partition it badly
 
 b) Select the minimum element of the sequence
 - Minimum element so it will partition every element to one list,
@@ -84,11 +85,96 @@ b) Select the minimum element of the sequence
 - Example case: Any list
 
 c) Select the median element of the sequence
-- O(n^2) even with quick select algorithm
+- O(n log n) without the quick select algorithm
 - Example case: Any list
 
 d) Select an element that is greater than exactly 10% of the others
-- O(n^2) as even with the median it will be O(n^2)
-- Example case: 
+- O(n log n)
+- Example case: Any list
+
+Problem 3
+================
+- O(n^2) for a a list where the values below/above the mean is only 1 element and the rest of the elements are on the
+    other side of the mean
+
+Problem 4
+================
+- Average case will arise when considering that the green (optimal) part is selected 50% of the time
+- If we choose the worst part of the green area, we split the list into 0.75n
+- Can keep splitting (i.e. n + 0.75n + 0.75^2 n + ...)
+- Bounded by some constant c so n + 0.75n + 0.75^2 n + ... <= c.n
+- Hence O(n)
+
+Problem 5
+================
+- To sort 4329, 5169, 4321, 3369, 2121, 2099
+- Sort the numbers from the right to left
+- Now sort the last index
+- 4321, 2121, 4329, 5169, 3369, 2099
+- Sort the second-last index
+- 4321, 2121, 4329, 5169, 3369, 2099
+- Sort the 3rd-last index
+- 2099, 2121, 5169, 4321, 4329, 3369
+- Sort the first index
+- 2099, 2121, 3369, 4321, 4329, 5169
+
+Problem 6
+==============
+a) Worst-case: O(nm) where you have to loop through each letter and sort the n words
+    - Can be very inefficient if the words are of drastically different sizes so that most of the word would be done in
+        adding the padding to the words
+b)
+- Only apply radix sort to where the letter for a word exists.
+- Start from right to left again
+- If no letter exists, append to list and sort the remaining
+
+Problem 7
+==============
+function kClosestToMedian(list, k) 
+    n = list.length
+    median = quickSelect(list, sum(list) / n)
+    
+    # Find and keep index of median
+    for i = 1...n
+        if list[i] == median
+            medianPosition = i
+            break
+        end if
+    end for
+    
+    # Iterate over and convert elements to difference
+    for i = 1...n
+        if i != medianPosition
+            list[i] = list[i] - median
+        end if
+    end for
+    
+    # Remove median
+    list.remove(medianPosition)
+    
+    # Find kth order statistic
+    kthOrder = quickSelect(list)
+    
+    # Iterate over each element, if less than the kthOrder, add it to the output
+    output = []
+    for i = 1...n
+        if list[i] > kthOrder
+            output.append(list[i])
+        end if
+    end for
+    
+    # Add median back to output
+    for i = 1...output.length
+        output[i] += median
+    end for
+    
+end function
+
+Problem 8
+==============
+- O(nk) = O(k^2 * n/k)
+- n(1/2)^d = k
+- d = log(n/k)
+
 
 '''
