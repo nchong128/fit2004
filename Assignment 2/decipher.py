da4211fe61778463d5a00a83bd7423af543f013e
nchong128 <nchong128@gmail.com>
2019-04-11 15:09:20 +1000

    Add edge cases, solve edge cases, minor changes

--- Assignment 2/decipher.py
+++ Assignment 2/decipher.py
@@ -1,32 +1,20 @@
-'''
-
-Task 1: Finding the longest subsequence of common alphabets
-- Program will decipher the message from two encrypted texts (encrypted.txt)
-- function called messageFind
-- input size is O(n+m) -> program needs a worst case of O(nm) and space complexity of
-    -O(nm)
-- Can be upper and lower case only, NO SPACES, NO PUNCTUATIONS
-- Consider O(nm + k)
-
-Task 2: Words separating
-- Will break deciphered message into multiple words using the vocab, input from dictionary.txt
-- Function called wordBreak to break initial deciphered message into multiple words using the dictionary
-- Normal condition: break message into available words
-
-- worst case should be O(kM.NM)
-- k is the size of input string
-- space complexity is O(kM + NM)
-
-- NOTES
-- Size of two texts will not be the same
-- input file names may be different
-'''
 class Decipher:
     def __init__(self):
         self.message = ""
 
     def messageFind(self, inputFileName):
-        # TODO: Deal with edge cases, e.g no input, just one letter, multiple lengths, etc.
+        '''
+        This function finds the longest common subsequence of two texts based on the file's content
+        Time Complexity: O(nm) where n is the size of the first text and m is the size of the second text
+        Space Complexity: O(nm)
+        Error handle:
+        Parameter:
+        Return:
+        Pre-requisite:
+        '''
+        if len(inputFileName) == 0:
+            return
+
         # Retrieve words
         inputFile = open(inputFileName)
         firstWord = inputFile.readline().strip()
@@ -45,41 +33,47 @@ class Decipher:
         # Tabulate our results table of (firstLen + 1) rows and (secondLen+1) columns
         numOfRows = firstLen + 1
         numOfCols = secondLen + 1
-        memoTable = [[None for i in range(numOfCols)] for j in range(numOfRows)]
+        DP = [[None for i in range(numOfCols)] for j in range(numOfRows)]
 
-        # Fill every element table based on recurrence relation
+        # Fill every element in the table based on recurrence relation
         for i in range(numOfRows):
             for j in range(numOfCols):
                 if i == 0 or j == 0:
-                    memoTable[i][j] = 0
+                    DP[i][j] = 0
                 elif firstWord[i-1] == secondWord[j-1]:
-                    memoTable[i][j] = 1 + memoTable[i-1][j-1]
+                    DP[i][j] = 1 + DP[i-1][j-1]
                 else:
-                    memoTable[i][j] = max(memoTable[i-1][j], memoTable[i][j-1])
+                    DP[i][j] = max(DP[i-1][j], DP[i][j-1])
 
-        ## Message retrieval
+        # Message retrieval
         # Begin at bottom-right most element and iterate backwards until word is generated
         rowPos = numOfRows - 1
         colPos = numOfCols - 1
 
         while rowPos > 0 and colPos > 0:
-            if (memoTable[rowPos-1][colPos] == memoTable[rowPos][colPos-1]) and (memoTable[rowPos-1][colPos] != memoTable[rowPos][colPos]):
+            if (DP[rowPos-1][colPos] == DP[rowPos][colPos-1]) and (DP[rowPos-1][colPos] != DP[rowPos][colPos]):
                 # Element above and element up have the same values AND they're different to current element
                 # Add to string and move up diagonally
                 self.message = firstWord[rowPos-1] + self.message
                 rowPos -= 1
                 colPos -= 1
-            elif memoTable[rowPos-1][colPos] > memoTable[rowPos][colPos - 1]:
+            elif DP[rowPos-1][colPos] > DP[rowPos][colPos - 1]:
                 # Element above is higher in value than element at left => Move up
                 rowPos -= 1
             else:
+                # Move left
                 colPos -= 1
 
-    def printTable(self,table):
-        for row in table:
-            print(row)
-
     def wordInDict(self, dictList, word):
+        '''
+        Time Complexity:
+        Space Complexity:
+        Error handle:
+        Parameter:
+        Return:
+        Pre-requisite:
+        '''
+
         # Returns the index of a given word (+ 1) if a word is found in the dictionary
         # -1 otherwise
         location = -1
@@ -90,6 +84,14 @@ class Decipher:
         return -1
 
     def largestWordInList(self, list):
+        '''
+        Time Complexity:
+        Space Complexity:
+        Error handle:
+        Parameter:
+        Return:
+        Pre-requisite:
+        '''
         largestWord = list[0]
 
         for word in list:
@@ -99,23 +101,27 @@ class Decipher:
         return largestWord
 
     def wordBreak(self, dictionaryFileName):
-        # Courtesy of Shams
-        # If dictionary file is not given, return
+        '''
+        Time Complexity:
+        Space Complexity:
+        Error handle:
+        Parameter:
+        Return:
+        Pre-requisite:
+        '''
         if len(dictionaryFileName) == 0:
             return
 
         # Get the words from the dictionary file and place into a list
         dictFile = open(dictionaryFileName)
-        dictContent = dictFile.read().split("\n")
+        dictContent = []
+        for line in dictFile:
+            line = line.strip()
+            if (len(line) > 0) and (len(line) < len(self.message)) :
+                dictContent.append(line)
         dictFile.close()
 
-        # Filter any empty strings
-        filteredDictContent = []
-        for entry in dictContent:
-            if len(entry) > 0:
-                filteredDictContent.append(entry)
-        dictContent = filteredDictContent
-
+        # If dictionary file is empty
         if len(dictContent) == 0:
             return
 
@@ -125,14 +131,14 @@ class Decipher:
 
         # Make memoization table based prev values
         numOfCols = len(self.message) + 1
-        memoTable = [[0 for i in range(numOfCols)] for j in range(numOfRows)]
+        DP = [[0 for i in range(numOfCols)] for j in range(numOfRows)]
 
         # Loop over every row and col
         for i in range(numOfRows):
             for j in range(numOfCols):
                 # Base case
                 if i == 0 or j == 0 :
-                    memoTable[i][j] = 0
+                    DP[i][j] = 0
                     continue
 
                 # See if the last i elements form a word which is in the dictionary
@@ -140,55 +146,61 @@ class Decipher:
 
                 if location != -1:
                     # Jackpot: Fill in the last i elements with the index of the word
-                    for k in range(j, j-i, -1 ):
-                        memoTable[i][k] = location
+                    for k in range(j, j-i, -1):
+                        DP[i][k] = location
                 else:
                     # Word from last i elements do not fit the dictionary
-                    memoTable[i][j] = memoTable[i-1][j]
+                    DP[i][j] = DP[i-1][j]
 
-        # Iterate over last row and form sentence
+        # Get results from the last row
         output = ""
-        lastRow = memoTable[-1][1:]
+        lastRow = DP[-1][1:]
+
         lastNumber = lastRow[0]
+        wordLength = 0
+
         for i in range(0, len(lastRow)):
-            if lastRow[i] != lastNumber:
+            if (lastRow[i] != lastNumber) or (lastNumber != 0 and (wordLength >= len(dictContent[lastNumber - 1]))):
                 output += " "
                 lastNumber = lastRow[i]
+                wordLength = 0
             output += self.message[i]
+            wordLength += 1
 
         # Adjust class variable to output
         self.message = output
 
     def getMessage(self):
+        '''
+        Time Complexity:
+        Space Complexity:
+        Error handle:
+        Parameter:
+        Return:
+        Pre-requisite:
+        '''
         return self.message
 
-def test():
-    #TODO: NEED TO REMOVE
-    inputFile = 'Input/PERSONAL4.txt'
-    dictFile = 'Input/EMPTYDICT.txt'
-
-    test = Decipher()
-
-    test.messageFind(inputFile)
-
-    test.wordBreak(dictFile)
-
-    print(test.getMessage())
-
-
 def main():
-    # TODO CHANGE THE FILES BELOW TO 'encrypted.txt' and 'dictionary.txt'
+    # TODO: Change files to "The name of the file, contains two encrypted texts : "
+    # TODO: Change files to "The name of the dictionary file : "
     # Get files
-    encryptedFile = "Input/PERSONAL2.txt"
-    dictFile = "Input/PERSONALDICT2.txt"
+    encryptedFile = "Input/PERSONALFREE.txt"
+    dictFile = "Input/PERSONALDICTFREE.txt"
     print("---------------------------------------------------------------------")
+
     # Do operations
     decipher = Decipher()
+
+    # Find the encrypted message
     decipher.messageFind(encryptedFile)
     print("Deciphered message is " + decipher.getMessage())
+
+    # Break the encrypted message into words
     decipher.wordBreak(dictFile)
     print("True message is " + decipher.getMessage())
     print("---------------------------------------------------------------------")
+
     print("Program end")
 
 if __name__ == '__main__':
